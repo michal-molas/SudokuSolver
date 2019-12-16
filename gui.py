8bac90cdb41683d5505938fd3aadfcf9f8cb59c4
michal-molas <molas.mich@gmail.com>
2019-12-16 21:28:50 +0100

    numpad numbers work

--- gui.py
+++ gui.py
@@ -181,6 +181,15 @@ def set_num(events, board, picked):
                     pygame.K_7: 7,
                     pygame.K_8: 8,
                     pygame.K_9: 9,
+                    pygame.K_KP1: 1,
+                    pygame.K_KP2: 2,
+                    pygame.K_KP3: 3,
+                    pygame.K_KP4: 4,
+                    pygame.K_KP5: 5,
+                    pygame.K_KP6: 6,
+                    pygame.K_KP7: 7,
+                    pygame.K_KP8: 8,
+                    pygame.K_KP9: 9,
                     pygame.K_DELETE: 0,
                     pygame.K_BACKSPACE: 0
                 }
